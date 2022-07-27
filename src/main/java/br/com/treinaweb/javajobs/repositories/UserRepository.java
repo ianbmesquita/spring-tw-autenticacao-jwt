268dc1390bd21b0a406e91de695763270465c208
ianbmesquita <ianmesquitadb@gmail.com>
2022-07-27 20:19:27 -0300

    feat: Creating the User repository.

---
+++ src/main/java/br/com/treinaweb/javajobs/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package br.com.treinaweb.javajobs.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.com.treinaweb.javajobs.models.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByUsername(String username);
+}
